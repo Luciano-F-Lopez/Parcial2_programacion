568cceb009f0d79ceb26a51907da56e8a7ea4d67
Luciano-F-Lopez <lucianolopez717@gmail.com>
2024-11-29 01:05:35 -0300

    antes de empezar parcial

---
+++ src/lopez_luciano_p_parcial2/Lopez_Luciano_p_Parcial2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lopez_luciano_p_parcial2;
+
+/**
+ *
+ * @author MIPC
+ */
+public class Lopez_Luciano_p_Parcial2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
